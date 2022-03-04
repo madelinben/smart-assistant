9096b0f8dde1e03b5fca2232895d70e683db1ee7
madelinben <ben.madelin@ntlworld.com>
2022-03-04 18:09:39 +0000

    Implemented google speech recognition to process query.

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+import speech_recognition
+
 def respond(responseString):
     print(responseString)
 
 def process():
-    query = None
+    r = speech_recognition.Recognizer()
+
+    with speech_recognition.Microphone() as source:
+        print('Listening...')
+        r.pause_threshold = 1
+        r.energy_threshold = 50
+        audio = r.listen(source)
+
+    try:
+        print('Processing...')
+        query = r.recognize_google(audio, language='en-in')
+        print(f'Query: {query}.\n')
+    except Exception as exception:
+        print(exception)
+        print('Could you say that again please.')
+        return None
+    
     return query
 
 def listen():
-    command = process()
+    command = process().lower()
     
     if not command:
         respond('Could you say that again please.')
